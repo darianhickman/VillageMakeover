94b0276921e6aee805622888e972bda8f5bf3e1b
Michał Zieliński <michal@zielinscy.org.pl>
2014-10-24 10:13:30 +0200

    multiple items in coin dialog

--- client/gameClasses/ui/CoinDialog.js
+++ client/gameClasses/ui/CoinDialog.js
@@ -21,28 +21,70 @@ var CoinDialog = Dialog.extend({
 			.translateTo(0, 21, 0)
 			.mount(this);
 
-        var itemEnt = new IgeUiElement()
-			.texture(ige.client.textures.marketItemBack)
-			.dimensionsFromTexture();
+        for(var i=0; i < 2; i++) { (function() {
+            var cash = (i + 1) * 10
+            var coins = (i + 1) * 10
 
-        var cashIcon = new IgeUiElement()
-			.id('fooCashIcon')
-			.texture(ige.client.textures.marketItemBack)
-			.dimensionsFromTexture()
-			.texture(ige.client.textures.star)
-			.center(10)
-			.bottom(5)
-			.mount(itemEnt);
+            var itemEnt = new IgeUiElement()
+			    .texture(ige.client.textures.marketItemBack)
+                .center((i - 1) * 150)
+                .top(20)
+			    .dimensionsFromTexture();
 
-        cashIcon.mouseUp(function() {
-            ige.input.stopPropagation();
-            ige.client.audio.select.play();
-            self.hide()
+            var cashIcon = new IgeUiElement()
+			    .id('coinDialog_cashIcon' + i)
+			    .texture(ige.client.textures.cash)
+			    .dimensionsFromTexture()
+			    .left(10)
+			    .bottom(5)
+			    .mount(itemEnt);
 
-            Buy.buy({cash: 10, coins: 10})
-        })
+            new IgeFontEntity()
+			    .id('coinDialog_cash' + i)
+			    .layer(2)
+			    .textAlignX(0)
+			    .colorOverlay('#000000')
+			    .nativeFont('10px Verdana')
+			    .nativeStroke(0.5)
+			    .nativeStrokeColor('#666666')
+			    .textLineSpacing(0)
+			    .text(cash + '')
+			    .width(20)
+			    .center(20)
+			    .mount(cashIcon);
 
-        itemEnt.center(-150).top(20);
-        itemEnt.mount(pageEnt)
+            var coinIcon = new IgeUiElement()
+			    .id('coinDialog_coinIcon' + i)
+			    .texture(ige.client.textures.coin)
+			    .dimensionsFromTexture()
+			    .left(50)
+			    .bottom(5)
+			    .mount(itemEnt);
+
+            new IgeFontEntity()
+			    .id('coinDialog_coin' + i)
+			    .layer(2)
+			    .textAlignX(0)
+			    .colorOverlay('#000000')
+			    .nativeFont('10px Verdana')
+			    .nativeStroke(0.5)
+			    .nativeStrokeColor('#666666')
+			    .textLineSpacing(0)
+			    .text(coins + '')
+			    .width(20)
+			    .center(20)
+			    .mount(coinIcon);
+
+            itemEnt.mouseUp(function() {
+                ige.input.stopPropagation();
+                ige.client.audio.select.play();
+                self.hide()
+
+                Buy.buy({cash: cash, coins: coins})
+            })
+
+            itemEnt.mount(pageEnt)
+        })() }
+
     },
 })
