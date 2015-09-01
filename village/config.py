993c12023ab4f7700906b449ca67ae43a9ebe414
Umut Nacak <umutnacak@gmail.com>
2015-09-01 23:34:53 +0300

    used docid instead of title

--- village/config.py
+++ village/config.py
@@ -8,11 +8,11 @@ from oauth2client.client import SignedJwtAssertionCredentials
 
 local_config = yaml.load(open(os.path.join(os.path.dirname(__file__), '../config.yaml')))
 
-catalog_name = 'Innovations Catalog Village Social OPERATIONAL DATA Devel'
-config_name = 'Village MakeOver Settings OPERATIONAL'
-goals_name = 'Village Makeover Operational Goals'
-news_feed_name = 'SOHIP Village News PRD'
-cash_bundle_name = 'Virtual Currency Cash Bundle Catalog Operational'
+catalog_docid = '1imTZeg1aM4d2cWXpPPfHbTpQP6KX2HyxydqJfcejHtY'
+config_docid = '1OEtPyvf7XGNxuVFqdHUCRbMl8rlmMS36eXPD6Kzdpvo'
+goals_docid = '15hzNCemsXZNPGVBo9ugWhLzFmg3qIrNu9BXTryL9BQI'
+news_feed_docid = '12xPbj-iVTmwnvsddxS3G9fJCR6YyfeZoobuCwTWHewM'
+cash_bundle_docid = '1nIosYSuk6EeZhRbrD0f426UsWSQD2MatyznBSBtUacs'
 
 is_login_necessary = False
 
@@ -54,15 +54,15 @@ def get_session():
         session = login()
     return session
 
-def get_sheet(name):
-    return login().open(name).sheet1.get_all_values()
+def get_sheet(docid):
+    return login().open_by_key(docid).sheet1.get_all_values()
 
-def get_worksheet(sheet_name,worksheet_name):
-    sheet = login().open(sheet_name)
+def get_worksheet(sheet_docid,worksheet_name):
+    sheet = login().open_by_key(sheet_docid)
     return sheet.worksheet(worksheet_name).get_all_values()
 
 def get_config_worksheet(worksheet_name):
-    data = get_worksheet(config_name,worksheet_name)
+    data = get_worksheet(config_docid,worksheet_name)
     headers = data[0]
     items = []
     for row in data[2:]:
@@ -71,7 +71,7 @@ def get_config_worksheet(worksheet_name):
     return items
 
 def get_goals_worksheet(worksheet_name):
-    data = get_worksheet(goals_name,worksheet_name)
+    data = get_worksheet(goals_docid,worksheet_name)
     headers = data[0]
     items = []
     for row in data[2:]:
@@ -80,7 +80,7 @@ def get_goals_worksheet(worksheet_name):
     return items
 
 def get_goals_settings(worksheet_name):
-    data = get_worksheet(goals_name,worksheet_name)
+    data = get_worksheet(goals_docid,worksheet_name)
     d = {}
     for row in data[1:]:
         d[row[0]] = row[1]
@@ -88,14 +88,14 @@ def get_goals_settings(worksheet_name):
 
 @memcached('config')
 def get_config():
-    data = get_sheet(config_name)
+    data = get_sheet(config_docid)
     d = {}
     for row in data[1:]:
         d[row[2]] = row[3]
     return d
 
 def get_news_feed():
-    data = get_sheet(news_feed_name)
+    data = get_sheet(news_feed_docid)
     headers = data[0]
     items = []
     for row in data[2:]:
@@ -111,7 +111,7 @@ def get_secret_key():
 
 @memcached('catalog')
 def get_catalog():
-    data = get_sheet(catalog_name)
+    data = get_sheet(catalog_docid)
     headers = data[0]
     items = []
     for row in data[2:]:
