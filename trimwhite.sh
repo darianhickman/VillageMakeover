28f57c1665b5c8b6badd1c1d2b3e8da6d2c1ce4d
darian.hickman <darian.hickman@villagethegame.com>
2016-06-30 16:40:26 -0400

    fixed ClientConfig.js and several filenames

--- trimwhite.sh
+++ trimwhite.sh
@@ -1,3 +1,3 @@
 find . -depth -name '* *' \
-| while IFS= read -r f ; do mv -i "$f" "$(dirname "$f")/$(basename "$f"|tr ' ' _)" ; done
+| while IFS= read -r f ; do mv -i "$f" "$(dirname "$f")/$(basename "$f"|tr ' ()-_' '')" ; done
 
