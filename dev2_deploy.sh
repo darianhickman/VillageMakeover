be760ccad1a5a3b323b947c97501a6d40fd145a4
darian.hickman <darian.hickman@villagethegame.com>
2016-06-30 17:29:13 -0400

    No more suppressing error/warning  messages

--- dev2_deploy.sh
+++ dev2_deploy.sh
@@ -6,4 +6,4 @@ cp config-dev.yaml config.yaml
 APPCFG=/Users/darianhickman/google-cloud-sdk/platform/google_appengine/appcfg.py
 
 
-$APPCFG update --application=villagegamedev2 --oauth2 . 2>&1 | grep -v 'Cannot upload both <filename>\.py and <filename>\.pyc' | grep -v 'Could not guess mimetype for'
+$APPCFG update --application=villagegamedev2  .
