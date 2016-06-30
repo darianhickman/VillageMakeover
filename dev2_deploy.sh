09c291ccd505dcff7f012daa105460beb7e4f740
darian.hickman <darian.hickman@villagethegame.com>
2016-06-30 17:07:26 -0400

    renamed files. and picking right appcfg.py in dev2_deploy

--- dev2_deploy.sh
+++ dev2_deploy.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 ./compile.sh
-cp config-dev.yaml config.yaml 
-
-if [ "$APPCFG" = "" ]; then
-    if which appcfg.py; then
-        APPCFG=appcfg.py
-    else
-        APPCFG=/Users/darianhickman/google-cloud-sdk/platform/google_appengine/appcfg.py
-
-    fi
-fi
-
 ./synclibs.sh
+
+cp config-dev.yaml config.yaml 
+APPCFG=/Users/darianhickman/google-cloud-sdk/platform/google_appengine/appcfg.py
+
+
 $APPCFG update --application=villagegamedev2 --oauth2 . 2>&1 | grep -v 'Cannot upload both <filename>\.py and <filename>\.pyc' | grep -v 'Could not guess mimetype for'
