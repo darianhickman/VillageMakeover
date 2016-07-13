a1ccdb5bdabc0c4d87293bfce1724c965b2df432
Umut Nacak <umutnacak@gmail.com>
2016-07-13 15:32:40 +0300

    fixed speed progress bugs

--- client/gameClasses/ui/BuyConfirm.js
+++ client/gameClasses/ui/BuyConfirm.js
@@ -1,27 +1,43 @@
 var BuyConfirm = Dialog.extend({
 	classId: 'BuyConfirm',
-    init: function (message, callback) {
+    init: function (message, callback, confirmOnly) {
         Dialog.prototype.init.call(this);
 
         var self = this;
 
         $("#buyConfirmMessage").html(message);
-        $("#buyConfirmYes")
-            .click(function() {
-                mixpanel.track("Confirm buy");
-                $("#buyConfirmYes").unbind("click");
-                $( "#buyConfirmDialog" ).dialog( "close" );
-                self.closeMe();
-                callback();
-            });
+        if(confirmOnly !== null && confirmOnly !== undefined && confirmOnly === true){
+            $("#buyConfirmYes").hide();
+            $("#buyConfirmNo").hide();
+            $("#buyConfirmOK")
+                .click(function() {
+                    mixpanel.track("Confirm dialog");
+                    $( "#buyConfirmDialog" ).dialog( "close" );
+                    self.closeMe();
+                })
+                .show();
+        }else{
+            $("#buyConfirmOK").hide();
 
-        $("#buyConfirmNo")
-            .click(function() {
-                mixpanel.track("Cancel buy");
-                $("#buyConfirmYes").unbind("click");
-                $( "#buyConfirmDialog" ).dialog( "close" );
-                self.closeMe();
-            });
+            $("#buyConfirmYes")
+                .click(function() {
+                    mixpanel.track("Confirm buy");
+                    $("#buyConfirmYes").unbind("click");
+                    $( "#buyConfirmDialog" ).dialog( "close" );
+                    self.closeMe();
+                    callback();
+                })
+                .show();
+
+            $("#buyConfirmNo")
+                .click(function() {
+                    mixpanel.track("Cancel buy");
+                    $("#buyConfirmYes").unbind("click");
+                    $( "#buyConfirmDialog" ).dialog( "close" );
+                    self.closeMe();
+                })
+                .show();
+        }
 
         $( "#buyConfirmDialog" ).dialog({ resizable: false, draggable: true, closeOnEscape: false, width: 'auto', height: 'auto', modal: true, autoOpen: false, close: function( event, ui ) {$("#buyConfirmYes").unbind("click");self.closeMe();} });
         $( "#buyConfirmDialog" ).dialog( "open" );
