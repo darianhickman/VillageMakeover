c3222bd48f5daddcd39029bebf3cb4adaf4ced64
Darian Hickman <darian.hickman@villagethegame.com>
2016-08-11 14:41:10 -0700

    update_prod.sh

--- update_prod.sh
+++ update_prod.sh
@@ -1,1 +1,9 @@
-update_villagethegame111.sh
+#!/bin/bash
+./compile.sh
+./synclibs.sh
+
+cp config-prod.yaml config.yaml 
+export APPCFG=/Users/darianhickman/google-cloud-sdk/platform/google_appengine/appcfg.py
+
+
+$APPCFG update --application=villagethegame111  .
